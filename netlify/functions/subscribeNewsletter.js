10551501204db4d02df629aa46e4bd64614b1842
Florian <hi@florianpanchout.com>
2021-04-19 16:38:45 +0200

    Use Axios

--- netlify/functions/subscribeNewsletter.js
+++ netlify/functions/subscribeNewsletter.js
@@ -1,19 +1,12 @@
-var SibApiV3Sdk = require('sib-api-v3-sdk')
-var defaultClient = SibApiV3Sdk.ApiClient.instance
+const axios = require('axios')
 
-// Configure API key authorization: api-key
-var apiKey = defaultClient.authentications['api-key']
-apiKey.apiKey = process.env.SENDINBLUE_API_KEY
-
-var apiInstance = new SibApiV3Sdk.ContactsApi()
-
-var createContact = new SibApiV3Sdk.CreateContact() // CreateContact | Values to create a contact
-
-exports.handler = async function(event, context, callback) {
-  createContact = {
-    email: event.queryStringParameters.email,
-    listIds: [10],
-  }
-
-  return apiInstance.createContact(createContact)
+exports.handler = async function(event, context) {
+  let headers = new Headers()
+  headers.append('Content-Type', 'application/json')
+  headers.append('api-key', process.env.SENDINBLUE_API_KEY)
+  return axios.post(
+    'https://api.sendinblue.com/v3/contacts',
+    { email, listIds: [10] },
+    { header }
+  )
 }
