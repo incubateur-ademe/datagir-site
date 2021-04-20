40055378b1996ad0b4225b0ea1486036b753d2f3
Florian <hi@florianpanchout.com>
2021-04-20 08:45:14 +0200

    Add Env

--- netlify/functions/subscribeNewsletter.js
+++ netlify/functions/subscribeNewsletter.js
@@ -1,5 +1,19 @@
 const axios = require('axios')
 
+exports.handler = async function(event, context) {
+  axios
+    .post('https://api.sendinblue.com/v3/contacts', { email, listIds: [10]}, {headers: {Content-Type: 'application/json'} })
+    .then((response) => {
+      console.log(response.data.url)
+      console.log(response.data.explanation)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+const axios = require('axios')
+
 exports.handler = async function(event) {
   return axios
     .post(
@@ -9,7 +23,7 @@ exports.handler = async function(event) {
         headers: {
           'Content-Type': 'application/json',
           'api-key':
-            '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',
+            process.env.SENDINBLUE_API_KEY,
         },
       }
     )
@@ -29,7 +43,7 @@ exports.handler = async function(event) {
             headers: {
               'Content-Type': 'application/json',
               'api-key':
-                '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',
+                process.env.SENDINBLUE_API_KEY,
             },
           }
         )
