53080eafc82430f0c38777f084ff651575907ee3
Florian <hi@florianpanchout.com>
2021-04-20 08:56:14 +0200

    Fix

--- netlify/functions/subscribeNewsletter.js
+++ netlify/functions/subscribeNewsletter.js
@@ -1,19 +1,5 @@
 const axios = require('axios')
 
-exports.handler = async function(event, context) {
-  axios
-    .post('https://api.sendinblue.com/v3/contacts', { email, listIds: [10]}, {headers: {Content-Type: 'application/json'} })
-    .then((response) => {
-      console.log(response.data.url)
-      console.log(response.data.explanation)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-}
-
-const axios = require('axios')
-
 exports.handler = async function(event) {
   return axios
     .post(
@@ -22,8 +8,7 @@ exports.handler = async function(event) {
       {
         headers: {
           'Content-Type': 'application/json',
-          'api-key':
-            process.env.SENDINBLUE_API_KEY,
+          'api-key': process.env.SENDINBLUE_API_KEY,
         },
       }
     )
@@ -42,8 +27,7 @@ exports.handler = async function(event) {
           {
             headers: {
               'Content-Type': 'application/json',
-              'api-key':
-                process.env.SENDINBLUE_API_KEY,
+              'api-key': process.env.SENDINBLUE_API_KEY,
             },
           }
         )
