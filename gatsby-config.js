2f592ff6a112e748c12d3ab58a505794081cada0
Florian <hi@florianpanchout.com>
2021-05-11 12:28:18 +0200

    Add remark-attr

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-root-import`,
     `gatsby-plugin-sitemap`,
+    'gatsby-remark-attr',
     {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
