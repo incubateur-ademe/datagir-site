09c8358368430aeeda764698387811def3a8fa8e
Florian <hi@florianpanchout.com>
2023-07-26 08:52:35 +0200

    feat: add link to incubateur

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Web from 'src/components/layout/Web'
 
 import Landing from 'src/components/home/Landing'
 import Applications from 'src/components/Applications'
 
+const Text = styled.p`
+  font-size: 1.25rem;
+`
 export default function Index() {
   return (
     <Web title={'Datagir'}>
       <Landing />
       <Applications />
+      <Text>
+        Découvrez également{' '}
+        <a
+          href='https://beta.gouv.fr/startups/?incubateur=ademe'
+          target='_blank'
+        >
+          toutes les réalisations de l'incubateur
+        </a>
+        .
+      </Text>
     </Web>
   )
 }
